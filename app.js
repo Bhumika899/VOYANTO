d03bae59db09685443429028b95a98434b289953
bhumika_03 <bhumikasabharwal0@gmail.com>
2026-06-26 14:09:33 +0530

    ADD PROJECT FILES

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret: "mysupersecretcode"
+        secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
 }
@@ -64,7 +64,7 @@ store.on("error", (err) => {
 })
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
